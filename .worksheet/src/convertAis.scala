265524b4561507297bf7035b09cd05a7c36bbe5d
Ewan Keith <keith.ewan@yahoo.co.uk>
2018-06-09 21:10:11 +0100

    see previous message.

--- .worksheet/src/convertAis.scala
+++ .worksheet/src/convertAis.scala
@@ -1,18 +1,18 @@
-object convertAis {
+object convertAis {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(251); 
 
   // convert a single AIS message character to Decimal Byte
   def charToDecimalByte(character: Option[Char]): Option[Int] =
     character match {
       case Some(char) => Some(char.toByte.toInt)
       case None       => None
-    }
+    };System.out.println("""charToDecimalByte: (character: Option[Char])Option[Int]""");$skip(126); 
   
   // convert an 8 bit decimal (byte) to 6 bit decimal representation
-  def eightBitToSixBit(eightBit: Int): Int = eightBit
+  def eightBitToSixBit(eightBit: Int): Int = eightBit;System.out.println("""eightBitToSixBit: (eightBit: Int)Int""");$skip(97); 
 
   def messageToSixBit(message: String): List[Char] = {
     message.toList.map(char => char)
-  }
+  };System.out.println("""messageToSixBit: (message: String)List[Char]""")}
   
   
 }
