bbba386d84635d70f6543bba728d950c7778b6c3
ewan-keith <keith.ewan@yahoo.co.uk>
2018-06-16 15:33:31 +0100

    removed all development guff

--- .worksheet/src/testWorksheet.scala
+++
@@ -1,3 +0,0 @@
-object testWorksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(66); 
-  println("Welcome to the Scala worksheet")}
-}
