1a119d2a2a69b0f007a5666ed9b9228e3cc964b0
ewan-keith <keith.ewan@yahoo.co.uk>
2018-07-04 21:10:52 +0100

    added checksum validity test unit tests

--- src/test/scala/sentence/AisSentenceValiditySpec.scala
+++ src/test/scala/sentence/AisSentenceValiditySpec.scala
@@ -5,17 +5,17 @@ import org.scalatest.FlatSpec
 import scala.util.{ Try, Success, Failure }
 
 class AisSentenceValiditySpec extends FlatSpec {
-  
-    val validAivdm: List[String] = Source
+
+  val validAivdm: List[String] = Source
     .fromURL(getClass.getResource("/nmea-sample.txt"))
     .getLines.toList
-    
-      val testFailure = Failure(
+
+  val testFailure = Failure(
     new Exception("Invalid AIS Sentence: test message"))
-    
-      val invalidLength = List("a,b,c,d,e,f", "a,v,d", "1,2,3,4,5,6,7,8", "test")
-      
-     "tryLengthSeven" should "return Success(msg) for 7 field sentences" in {
+
+  val invalidLength = List("a,b,c,d,e,f", "a,v,d", "1,2,3,4,5,6,7,8", "test")
+
+  "tryLengthSeven" should "return Success(msg) for 7 field sentences" in {
     validAivdm.foreach(
       sntnc => {
         AisSentenceValidity.tryLengthSeven(Try(sntnc)) match {
@@ -25,8 +25,8 @@ class AisSentenceValiditySpec extends FlatSpec {
         }
       })
   }
-    
-    it should "return Failure(e) for non-7 field messages" in {
+
+  it should "return Failure(e) for non-7 field messages" in {
     invalidLength.foreach(
       sntnc => {
         AisSentenceValidity.tryLengthSeven(Try(sntnc)) match {
@@ -49,11 +49,11 @@ class AisSentenceValiditySpec extends FlatSpec {
         "Invalid AIS Sentence: test message")
     }
   }
-  
-        val invalidIdent = 
-          List("test,123,456", "!AIVD0,123,456", "!AVDM,1,1", "$AIVDM,1")
-      
-     "tryValidIdent" should "return Success(msg) for message with valid ident" in {
+
+  val invalidIdent =
+    List("test,123,456", "!AIVD0,123,456", "!AVDM,1,1", "$AIVDM,1")
+
+  "tryValidIdent" should "return Success(msg) for message with valid ident" in {
     validAivdm.foreach(
       sntnc => {
         AisSentenceValidity.tryValidIdent(Try(sntnc)) match {
@@ -63,8 +63,8 @@ class AisSentenceValiditySpec extends FlatSpec {
         }
       })
   }
-    
-    it should "return Failure(e) for invalid ident messages" in {
+
+  it should "return Failure(e) for invalid ident messages" in {
     invalidIdent.foreach(
       sntnc => {
         AisSentenceValidity.tryValidIdent(Try(sntnc)) match {
@@ -87,26 +87,22 @@ class AisSentenceValiditySpec extends FlatSpec {
         "Invalid AIS Sentence: test message")
     }
   }
-  
-          val invalidChecksum = 
-          List("test*O2", "test02", "test*0", "123*012")
-          
-               "tryChecksumPresent" should "return Success(msg) for message with nmea checksum" in {
+
+  val absentChecksum = List("test*O2", "test02", "test*0", "123*012")
+
+  "tryChecksumPresent" should "return Success(msg) for message with nmea checksum" in {
     validAivdm.foreach(
       sntnc => {
         AisSentenceValidity.tryChecksumPresent(Try(sntnc)) match {
           case Success(msg) => assert(msg === sntnc)
-          case Failure(e) => {
-            println(sntnc)
-          
+          case Failure(e) =>
             fail("tryChecksumPresent returned Failure for valid sentence")
-          }
         }
       })
   }
-    
-    it should "return Failure(e) for invalid or missing checksum" in {
-    invalidChecksum.foreach(
+
+  it should "return Failure(e) for  mis-formatted or missing checksum" in {
+    absentChecksum.foreach(
       sntnc => {
         AisSentenceValidity.tryChecksumPresent(Try(sntnc)) match {
           case Success(msg) =>
@@ -124,6 +120,45 @@ class AisSentenceValiditySpec extends FlatSpec {
     triedChksmPresent match {
       case Success(msg) =>
         fail("tryChecksumPresent returned Success when input Failure(e)")
+      case Failure(e) => assert(e.getMessage ===
+        "Invalid AIS Sentence: test message")
+    }
+  }
+
+  val invalidChecksum = List(
+    "!AIVDM,1,1,,A,13HOI:0P0000VOHLCnHQKwvL05Ip,0*24",
+    "!AIVDM,1,1,,A,133w;`PP00PCqghMcqNqdOvPR5Ip,0*00")
+
+  "tryChecksumValid" should "return Success(msg) for valid nmea checksum" in {
+    validAivdm.foreach(
+      sntnc => {
+        AisSentenceValidity.tryChecksumValid(Try(sntnc)) match {
+          case Success(msg) => assert(msg === sntnc)
+          case Failure(e) =>
+            fail("tryChecksumValid returned Failure for valid checksum")
+        }
+      })
+  }
+
+  it should "return Failure(e) for invalid checksum" in {
+    invalidChecksum.foreach(
+      sntnc => {
+        AisSentenceValidity.tryChecksumValid(Try(sntnc)) match {
+          case Success(msg) =>
+            fail("tryChecksumValid returned Success for invalid or missing checksum")
+          case Failure(e) =>
+            assert(e.getMessage ===
+              "Invalid AIS Sentence: Checksum is invalid")
+        }
+      })
+  }
+
+  it should "pass on Failure(e) unchanged" in {
+    val triedChksmValid = AisSentenceValidity.tryChecksumValid(testFailure)
+
+    triedChksmValid match {
+      case Success(msg) =>
+        fail("tryChecksumValid returned Success when input Failure(e)")
       case Failure(e) => assert(e.getMessage ===
         "Invalid AIS Sentence: test message")
     }
